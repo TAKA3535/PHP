e60016e3fce37164801d4ce440374f058d0f5113
taka <neithi.gensin@gmail.com~>
2023-01-23 16:01:23 +0900

    miniTwitter開発

---
+++ miniTwitter/post/join/thanks.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>登録完了</title>
+</head>
+<body>
+    <!-- P246 -->
+    <p>ユーザー登録が完了しました</p>
+    <p><a href="../">ログインする</a></p>
+</body>
+</html>
